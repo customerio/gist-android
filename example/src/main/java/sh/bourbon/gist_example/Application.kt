fb5a881ae644af34f46e6223e3c929238aca9098
Daniel Gauci <daniel.gauci@gmail.com>
2020-04-22 01:24:21 +0200

    Update README and example to use getInstance()

--- example/src/main/java/sh/bourbon/gist_example/Application.kt
+++ example/src/main/java/sh/bourbon/gist_example/Application.kt
@@ -8,10 +8,12 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val gistSdk = GistSdk.getInstance()
+
         // Initialize Gist SDK with organization ID
-        GistSdk.init(this, BuildConfig.ORGANIZATION_ID)
+        gistSdk.init(this, BuildConfig.ORGANIZATION_ID)
 
         // Set current user ID
-        GistSdk.setUserToken(BuildConfig.USER_ID)
+        gistSdk.setUserToken(BuildConfig.USER_ID)
     }
 }
