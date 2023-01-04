8f8657d4898b75e063939d67cc8cd1c98e781cbd
Bernard Gatt <bernard@customer.io>
2023-01-04 16:03:28 +0100

    Renamed SiteId header

--- gist/src/main/java/build/gist/data/NetworkUtilities.kt
+++ gist/src/main/java/build/gist/data/NetworkUtilities.kt
@@ -2,7 +2,7 @@ package build.gist.data
 
 class NetworkUtilities {
     companion object {
-        internal const val CIO_SITE_ID_HEADER = "X-Gist-CIO-Site-Id"
+        internal const val CIO_SITE_ID_HEADER = "X-CIO-Site-Id"
         internal const val USER_TOKEN_HEADER = "X-Gist-User-Token"
         internal const val CIO_DATACENTER_HEADER = "X-CIO-Datacenter"
     }
