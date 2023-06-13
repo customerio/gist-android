df8aba80300d70f3bd29c0eaa87372d34c6d7cdb
Bernard Gatt <bernard@customer.io>
2023-06-13 16:11:43 +0200

    Fixes an issue when the local queue is being cleared when no content is returned.

--- gist/src/main/java/build/gist/data/listeners/Queue.kt
+++ gist/src/main/java/build/gist/data/listeners/Queue.kt
@@ -72,6 +72,9 @@ class Queue : GistListener {
             try {
                 Log.i(GIST_TAG, "Fetching user messages")
                 val latestMessagesResponse = gistQueueService.fetchMessagesForUser()
+                // If there's no change (304), move on.
+                if (latestMessagesResponse.code() == 304) { return@launch }
+
                 // To prevent us from showing expired / revoked messages, clear user messages from local queue.
                 clearUserMessagesFromLocalStore()
                 if (latestMessagesResponse.code() == 204) {
